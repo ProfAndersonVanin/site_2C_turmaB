bd9501afe3157dd4afe1ff005788811d9aca9c13
profandersonvanin <profandersonvanin@gmail.com>
2024-09-02 08:50:35 -0300

    Criação da Session

--- painel/index.php
+++ painel/index.php
@@ -1,6 +1,14 @@
 <?php
-    include 'conecta.php';
+    session_start();
+    if((!isset($_SESSION['id']) == true) and (!isset($_SESSION['nome']) == true) and (!isset($_SESSION['email']) == true)){
+        unset($_SESSION['id']);
+        unset($_SESSION['nome']);
+        unset($_SESSION['email']);
+        header('Location: ../index.html');
+    }
 
+
+    include 'conecta.php';
     include 'menu.php';
 ?>
 
